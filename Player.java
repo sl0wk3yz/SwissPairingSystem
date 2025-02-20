6fcf996218ba716bcc4f050bcc063ec774bd7671
sl0wk3yz <lwazinkoe@gmail.com>
2025-02-20 17:33:14 +0200

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package swisspairings;
+
+/**
+ *
+ * @author lwazi
+ */
+class Player {
+    private String name;
+    private int rating;
+
+    public Player(String name, int rating) {
+        this.name = name;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+}
---
+++ SwissPairings.java
@@ -0,0 +1,44 @@
+package swisspairings;
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Lwazi Selepe
+ */
+public class SwissPairings {
+
+    /**
+     * @param args the command line arguments
+     */
+
+   public static void main(String[] args) {
+        // Creating an ArrayList to store players
+        ArrayList<Player> players = new ArrayList<>();
+
+        // Adding players to the ArrayList
+        int isTrue;
+        do{
+            isTrue = JOptionPane.showConfirmDialog(null, "would you like to add a player?");
+            if(isTrue==JOptionPane.YES_OPTION){
+                String playerName = JOptionPane.showInputDialog("Enter player name");
+            int playerRating = Integer.parseInt(JOptionPane.showInputDialog("Enter player elo"));
+            players.add(new Player(playerName, playerRating));
+       
+            }
+        }while(isTrue == JOptionPane.YES_OPTION);
+
+        // Accessing player information
+        for (int i = 0; i < players.size(); i++) {
+        Player player = players.get(i);
+        System.out.println("Name: " + player.getName() + "\t Rating: " + player.getRating());
+}
+
+    }
+}
+
+
+    
+    
+
